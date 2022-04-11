25d95ea803202b388ee938122caeb19eb3d406c7
Matthew Dolan <MatthewDolan@users.noreply.github.com>
2022-04-11 03:28:56 +0000

    Add `By` func

--- jitter.go
+++ jitter.go
@@ -1,5 +1,3 @@
-// I am not overly concerned about the vulnerability of the random used in the backoff algorithm.
-// nolint: gosec
 package jitter
 
 import (
@@ -9,10 +7,21 @@ import (
 
 // Delay returns a random duration between (delay * (1 - factor), delay].
 func Delay(delay time.Duration, factor float64) time.Duration {
-	maxJitter := time.Duration(float64(delay) * factor)
-
-	if maxJitter == 0 {
+	if factor <= 0.0 {
 		return delay
+	}
+
+	return By(delay, time.Duration(float64(delay)*factor))
+}
+
+// By returns a random duration between (delay-jitter, delay].
+func By(delay time.Duration, maxJitter time.Duration) time.Duration {
+	if maxJitter <= 0 {
+		return delay
+	}
+
+	if maxJitter > delay {
+		return UpTo(delay)
 	}
 
 	return delay - UpTo(maxJitter) + 1
@@ -20,5 +29,9 @@ func Delay(delay time.Duration, factor float64) time.Duration {
 
 // UpTo returns a random duration between (0, max].
 func UpTo(max time.Duration) time.Duration {
+	if max <= 0 {
+		return 1
+	}
+
 	return time.Duration(rand.Int63n(int64(max))) + 1
 }
